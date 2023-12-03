31d881e958b6c1cb322137f56ba8310d2cd82ccb
pravalgautam <pravalgautam79@gmail.com>
2023-12-03 18:42:33 +0530

    Initial Commit

---
+++ Maosam/MaosamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaosamApp.swift
+//  Maosam
+//
+//  Created by Praval Gautam on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MaosamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
